8e538065a5822d3eccd9874ef334e5d74f3c58aa
jayanta-kumar-roy <35413962+jayanta-kumar-roy@users.noreply.github.com>
2021-07-18 22:42:30 +0530

    models added

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 projectList=[
     {
         'id':'1',
@@ -15,22 +15,18 @@ projectList=[
     {
         'id':'3',
         'title':"Social Network",
-        'description':'Awesome open source project i am still working on'
+        'description':'Awesom e open source project i am still working on'
     },
 ]
 
 def projects(request):
-    page="Projects"
-    number = 10
-    context={'page': page,'number':number,'projects':projectList}
+    projects = Project.objects.all()
+    context={'projects':projects}
 
     return render(request,'projects/projects.html',context) 
 
 def project(request,pk):
-    projectObj = None
-    for i in projectList:
-        if i['id'] == pk:
-            projectObj = i
-
-    return render(request,'projects/single-project.html',{'project':projectObj})
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request,'projects/single-project.html',{'project':projectObj,})
 
