9f4902664423b975b43fc58e7529b1a0fff7846d
jayanta-kumar-roy <35413962+jayanta-kumar-roy@users.noreply.github.com>
2021-07-18 16:23:30 +0530

    dynamic content tested

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList=[
+    {
+        'id':'1',
+        'title':"ecommerce Website",
+        'description':'fully funtional ecommerce'
+    },
+    {
+        'id':'2',
+        'title':"Portfolio Website",
+        'description':'this is project where I built my portfolio'
+    },
+    {
+        'id':'3',
+        'title':"Social Network",
+        'description':'Awesome open source project i am still working on'
+    },
+]
+
 def projects(request):
-    return render(request,'projects/projects.html') 
+    page="Projects"
+    number = 10
+    context={'page': page,'number':number,'projects':projectList}
+
+    return render(request,'projects/projects.html',context) 
 
 def project(request,pk):
-    return render(request,'projects/single-project.html')
+    projectObj = None
+    for i in projectList:
+        if i['id'] == pk:
+            projectObj = i
+
+    return render(request,'projects/single-project.html',{'project':projectObj})
 
